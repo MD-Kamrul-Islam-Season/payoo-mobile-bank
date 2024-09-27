c652cad78be26caab3a54fd2df08772cb402966a
MD-Kamrul-Islam-Season <seasonmdkamrulislam@gmail.com>
2024-09-28 00:17:28 +0600

    3rd commit

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log("home added");
+//Add money to the account
+//step-1: add an eventhandler to add money button inside the form
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function (event) {
+    //prevent page reload after form submit
+    event.preventDefault();
+    //step-2: get money to be added to the account
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    console.log(addMoneyInput);
+    const pinNumberInput = document.getElementById("input-pin-number").value;
+    console.log(pinNumberInput);
+  });
